1640c794eed623914dbed16f42fd05cca1adb05b
PatrickRitskesSoftpak <patrick.ritskes@softpak.nl>
2019-09-20 14:06:14 +0200

    Save timereg to db

--- src/pages/time-registration/time-registration.ts
+++ src/pages/time-registration/time-registration.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup } from "@angular/forms";
+import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFireDatabase } from "angularfire2/database";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import moment from "moment";
 
@@ -15,18 +17,23 @@ export class TimeRegistrationPage implements OnInit {
   timeRegForm: FormGroup;
   totalWorkedHours: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase
+  ) {}
 
   ngOnInit() {
     this.totalWorkedHours = 0;
     this.timeRegForm = new FormGroup({
-      maandag: new FormControl(),
-      dinsdag: new FormControl(),
-      woensdag: new FormControl(),
-      donderdag: new FormControl(),
-      vrijdag: new FormControl(),
-      zaterdag: new FormControl(),
-      zondag: new FormControl()
+      maandag: new FormControl(0),
+      dinsdag: new FormControl(0),
+      woensdag: new FormControl(0),
+      donderdag: new FormControl(0),
+      vrijdag: new FormControl(0),
+      zaterdag: new FormControl(0),
+      zondag: new FormControl(0)
     });
     moment.locale("nl");
     let startOfWeek = moment().startOf("isoWeek");
@@ -38,14 +45,15 @@ export class TimeRegistrationPage implements OnInit {
       day = day.clone().add(1, "d");
     }
 
-    const beginWeekDay = moment().weekday(1);
-    const endWeekDay = moment().weekday(5);
+    const beginWeekDay = moment().day(1);
+    const endWeekDay = moment().day(7);
     this.weekBeginAndEndDay =
       beginWeekDay.format("DD MMM") + " - " + endWeekDay.format("DD MMM Y");
     this.weekNumber = moment().week();
   }
 
   saveTimeReg() {
+    let uid = this.afAuth.auth.currentUser.uid;
     let workedHours = [];
     const rawValues = this.timeRegForm.getRawValue();
     Object.keys(rawValues).forEach(values => {
@@ -59,9 +67,30 @@ export class TimeRegistrationPage implements OnInit {
         .reduce(this.incrementSumValue)
         .toFixed(2);
     }
+    const saveToDB = {
+      [moment().year()]: {
+        [moment().week()]: {
+          days: rawValues
+        }
+      }
+    };
+    this.db.list(`/users/`).update(`${uid}/timeReg/`, saveToDB);
   }
 
   private incrementSumValue(previousValue: number, currentValue: number) {
     return previousValue + currentValue;
+  }
+
+  private sortDays(a, b) {
+    let daysOfWeek = {
+      maandag: 1,
+      dinsdag: 2,
+      woensdag: 3,
+      donderdag: 4,
+      vrijdag: 5,
+      zaterdag: 6,
+      zondag: 7
+    };
+    // return daysOfWeek.indexOf(a) - daysOfWeek.indexOf(b);
   }
 }
