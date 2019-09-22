5bf2c7b483284504d68a23a8c69532e5879084a5
patrickritskes <patrickritskes@gmail.com>
2019-09-22 02:56:47 +0200

    Reset empty values when navigating

--- src/pages/time-registration/time-registration.ts
+++ src/pages/time-registration/time-registration.ts
@@ -12,12 +12,13 @@ import moment from "moment";
 })
 export class TimeRegistrationPage implements OnInit {
   weekDays: Array<Date> = [];
+  currentYear: number;
+  currentMonth: number;
   weekNumber: any;
   weekBeginAndEndDay: any;
   timeRegForm: FormGroup;
   totalWorkedHours = 0;
   currentDate = moment();
-  currentYear: number;
 
   constructor(
     public navCtrl: NavController,
@@ -33,12 +34,12 @@ export class TimeRegistrationPage implements OnInit {
 
   ngOnInit() {
     this.timeRegForm = new FormGroup({
-      maandag: new FormControl(),
-      dinsdag: new FormControl(),
-      woensdag: new FormControl(),
-      donderdag: new FormControl(),
-      vrijdag: new FormControl(),
-      zaterdag: new FormControl()
+      maandag: new FormControl('00:00'),
+      dinsdag: new FormControl('00:00'),
+      woensdag: new FormControl('00:00'),
+      donderdag: new FormControl('00:00'),
+      vrijdag: new FormControl('00:00'),
+      zaterdag: new FormControl('00:00')
     });
   }
 
@@ -69,16 +70,18 @@ export class TimeRegistrationPage implements OnInit {
     }
   }
 
-  saveTimeReg(year: number, weekNumber: number) {
+  saveTimeReg(year: number, month: number, weekNumber: number) {
     let uid = this.afAuth.auth.currentUser.uid;
     let workedHours = [];
     const rawValues = this.timeRegForm.getRawValue();
     Object.keys(rawValues).forEach(values => {
       if (rawValues[values]) {
+        // parse string to float
         let replacedValue = rawValues[values].replace(":", ".");
         workedHours.push(+replacedValue);
       }
     });
+    // Calculate week workinghours when save to db
     if (workedHours.length > 0) {
       this.totalWorkedHours = workedHours
         .reduce(this.incrementSumValue)
@@ -86,7 +89,7 @@ export class TimeRegistrationPage implements OnInit {
     }
     this.db
       .list(`/users/`)
-      .update(`${uid}/timeReg/${year}/${weekNumber}/days/`, rawValues);
+      .update(`${uid}/timeReg/${year}/${month}/${weekNumber}/days/`, rawValues);
   }
 
   private incrementSumValue(previousValue: number, currentValue: number) {
@@ -105,44 +108,47 @@ export class TimeRegistrationPage implements OnInit {
       );
     }
     this.currentYear = moment(endOfWeek).year();
+    this.currentMonth = moment(endOfWeek).month() + 1;
     this.weekDays = days;
     this.weekNumber = this.currentDate.week();
     this.weekBeginAndEndDay = this.weekDays[0] + " - " + this.weekDays[5];
-    this.getTimeReg();
+    this.getTimeReg(this.currentYear, this.currentMonth, this.weekNumber)
   }
 
-  private getTimeReg() {
+  private getTimeReg(year: number, month: number, weekNumber: number) {
     let uid = this.afAuth.auth.currentUser.uid;
     let workedHours = [];
-    let dbRef = this.db.database.ref(`users/${uid}/timeReg/`);
-    dbRef.on("value", snapshot => {
-      const dataSnapshot = snapshot.val();
-      Object.keys(dataSnapshot).forEach(year => {
-        if (+year === this.currentDate.year()) {
-          Object.keys(dataSnapshot[year]).forEach(weekNumber => {
-            if (+weekNumber === this.currentDate.week()) {
-              const filledTimeReg = dataSnapshot[year][weekNumber].days;
-              this.timeRegForm = new FormGroup({
-                maandag: new FormControl(filledTimeReg.maandag || ''),
-                dinsdag: new FormControl(filledTimeReg.dinsdag || ''),
-                woensdag: new FormControl(filledTimeReg.woensdag || ''),
-                donderdag: new FormControl(filledTimeReg.donderdag || ''),
-                vrijdag: new FormControl(filledTimeReg.vrijdag || ''),
-                zaterdag: new FormControl(filledTimeReg.zaterdag || '')
-              });
-              Object.keys(filledTimeReg).forEach(days => {
-                if (filledTimeReg[days]) {
-                  let replacedValue = filledTimeReg[days].replace(":", ".");
-                  workedHours.push(+replacedValue);
-                }
-              })
-              this.totalWorkedHours = workedHours
-                .reduce(this.incrementSumValue)
-                .toFixed(2);
-            }
-          })
+    // Query DB based on incoming year, month and weeknumber
+    let dbRef = this.db.database.ref(`users/${uid}/timeReg/${year}/${month}/${weekNumber}/days/`);
+    dbRef.on("value", dataSnapshot => {
+      const dayWithValues = dataSnapshot.val();
+      if (dayWithValues) {
+        this.timeRegForm.setValue({
+          maandag: dayWithValues.maandag || '00:00',
+          dinsdag: dayWithValues.dinsdag || '00:00',
+          woensdag: dayWithValues.woensdag || '00:00',
+          donderdag: dayWithValues.donderdag || '00:00',
+          vrijdag: dayWithValues.vrijdag || '00:00',
+          zaterdag: dayWithValues.zaterdag || '00:00'
+        });
+        // Calculate week working hours
+        Object.keys(dayWithValues).forEach(days => {
+          if (dayWithValues[days]) {
+            let replacedValue = dayWithValues[days].replace(":", ".");
+            workedHours.push(+replacedValue);
+          }
+        })
+        if (workedHours && workedHours.length > 0) {
+          this.totalWorkedHours = workedHours
+            .reduce(this.incrementSumValue)
+            .toFixed(2);
         }
-      })
+      }
+      // Reset empty form controls when navigating
+      else {
+        this.timeRegForm.reset('00:00')
+        this.totalWorkedHours = 0
+      }
     });
   }
 }
